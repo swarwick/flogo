8421b57a5a2d01435527c7da3f3665c608a24eec
swarwick <swarwick@tibco.com>
2019-04-08 15:54:20 -0400

    Starting to clean up tests and make them more generic

--- trigger/wits0/trigger_test.go
+++ trigger/wits0/trigger_test.go
@@ -31,26 +31,31 @@ const testConfig string = `{
             "ref": "github.com/TIBCOSoftware/flogo-contrib/action/flow",
             "data": {
               "flowURI": "res://flow:query"
-            },
-            "mappings": {
-              "input": [
-                {
-                  "mapTo": "request",
-                  "type": "assign",
-                  "value": "$.args"
-                }
-              ],
-              "output": [
-                {
-                  "mapTo": "data",
-                  "type": "assign",
-                  "value": "$.resp"
-                }
-              ]
             }
           },
 		"settings": {
 			"SerialPort": "/dev/ttyUSB0",
+			"HeartBeatValue": "&&\n0111-9999\n!!",
+			"PacketHeader": "&&",
+			"PacketFooter": "!!",
+			"LineSeparator":"\r\n"
+		}
+	}]
+}`
+
+const testConfigBaseSerialPort string = `{
+	"id": "wits0",
+	"settings": {
+	},
+	"handlers": [{
+		"action": {
+            "ref": "github.com/TIBCOSoftware/flogo-contrib/action/flow",
+            "data": {
+              "flowURI": "res://flow:query"
+            }
+          },
+		"settings": {
+			"SerialPort": "/dev/dummy",
 			"HeartBeatValue": "&&\n0111-9999\n!!",
 			"PacketHeader": "&&",
 			"PacketFooter": "!!",
@@ -104,28 +109,50 @@ func (tr *TestAction) IOMetadata() *data.IOMetadata {
 }
 
 func TestConnect(t *testing.T) {
+	trg, config := createTrigger(t, testConfig)
+	initialTrigger(t, trg, config)
+	runTrigger(5, trg)
+}
+
+func TestBadSerialPort(t *testing.T) {
+	trg, config := createTrigger(t, testConfigBaseSerialPort)
+	initialTrigger(t, trg, config)
+	runTrigger(5, trg)
+}
+
+func runTrigger(timeout int, trg trigger.Trigger) {
+	go func() {
+		time.Sleep(time.Second * time.Duration(timeout))
+		trg.Stop()
+	}()
+
+	trg.Start()
+}
+
+func createTrigger(t *testing.T, conf string) (trigger.Trigger, trigger.Config) {
 	log.SetLogLevel(logger.DebugLevel)
-	// New factory
 	md := trigger.NewMetadata(getJSONMetadata())
 	f := NewFactory(md)
-
+	config := trigger.Config{}
 	if f == nil {
 		t.Fail()
-		return
+		return nil, config
 	}
 
-	// New Trigger
-	config := trigger.Config{}
-	jsonErr := json.Unmarshal([]byte(testConfig), &config)
+	jsonErr := json.Unmarshal([]byte(conf), &config)
 	if jsonErr != nil {
 		log.Error(jsonErr)
 		t.Fail()
-		return
+		return nil, config
 	}
 	trg := f.New(&config)
+	return trg, config
+}
 
+func initialTrigger(t *testing.T, trg trigger.Trigger, config trigger.Config) trigger.Initializable {
 	if trg == nil {
 		t.Fail()
+		return nil
 	}
 	newTrg, _ := trg.(trigger.Initializable)
 
@@ -137,17 +164,10 @@ func TestConnect(t *testing.T) {
 		log.Infof("hConfig: %v", hConfig)
 		log.Infof("trg.Metadata().Output: %v", trg.Metadata().Output)
 		log.Infof("trg.Metadata().Reply: %v", trg.Metadata().Reply)
-
 		handler := trigger.NewHandler(hConfig, action, trg.Metadata().Output, trg.Metadata().Reply, runner)
 		initCtx.handlers = append(initCtx.handlers, handler)
 	}
 
 	newTrg.Initialize(initCtx)
-
-	go func() {
-		time.Sleep(time.Second * 5)
-		trg.Stop()
-	}()
-
-	trg.Start()
+	return newTrg
 }
